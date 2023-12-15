fceebe8abd2c6fa6fb0ca8a70f7e341c2dcbe95a
michael-glum <glummb@miamioh.edu>
2023-12-15 16:16:54 -0500

    Implementing mandatory privacy webhooks

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -14,19 +14,58 @@ export const action = async ({ request }) => {
   switch (topic) {
     case "APP_UNINSTALLED":
       if (session) {
-        await db.session.deleteMany({ where: { shop } });
+        await db.session.delete({ where: { shop } });
       }
+      throw new Response("Session deleted", { status: 200 });
 
-      break;
     case "BULK_OPERATIONS_FINISH":
       if (topic && shop && session) {
         await processBulkOrdersWebhook(topic, shop, session, clonedRequest);
       }
+      throw new Response("Bulk orders webhook reached", { status: 200 });
 
-      break;
     case "CUSTOMERS_DATA_REQUEST":
+      try {
+        const email = payload?.customer?.email
+        let message = "Data not found";
+        let storedData = null;
+        if (email != null) {
+          storedData = await db.user.findUnique({ where: { email: email }, select: { email: true }})
+          if (storedData != null) {
+            message = "Customer data found"
+          }
+        }
+        throw new Response(message, { data: storedData, status: 200 });
+      } catch (error) {
+        throw new Response("Data not found", { status: 200 })
+      }
+
     case "CUSTOMERS_REDACT":
+      try {
+        const email = payload?.customer?.email
+        let message = "Data not found";
+        let deletedCustomer = null;
+        if (email != null) {
+          deletedCustomer = await db.user.delete({ where: { email: email }})
+          if (deletedCustomer != null) {
+            message = "Customer data redacted"
+          }
+        }
+        throw new Response(message, { status: 200 });
+      } catch (error) {
+        throw new Response("Data not found", { status: 200 })
+      }
+
     case "SHOP_REDACT":
+      try {
+        if (shop) {
+          await db.partnership.delete({ where: { shop } });
+        }
+        throw new Response("Store data erased", { status: 200 });
+      } catch (error) {
+        throw new Response("Store data not found", { status: 200 });
+      }
+
     default:
       throw new Response("Unhandled webhook topic", { status: 404 });
   }
